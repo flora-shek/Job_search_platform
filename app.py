39ca3c28de374d59ac4dec446ffcd6ab62535478
Shekinah Florance <84413589+flora-shek@users.noreply.github.com>
2024-04-13 07:44:53 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def hello_jovian():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0',port=8000,debug=True)
+    app.run(host = '0.0.0.0',port=4530,debug=True)
