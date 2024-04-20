4df9d6f300732bd26fc8925f4cfe4bda7e3d4f25
Shekinah Florance <84413589+flora-shek@users.noreply.github.com>
2024-04-20 16:00:34 +0000

    added  application forms

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 from database import load_jobs_from_db, load_job_from_db
+
+
 
 app = Flask(__name__)
   
@@ -22,6 +24,13 @@ def show_job(id):
     return "Not Found",404
   return render_template("jobpage.html",job = job)
   
-
+@app.route("/job/<id>/apply", methods=[ "post"])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  #store the data
+  #send an email
+  #display acknowledgement
+  return render_template("application.html",application = data,job = job)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=4530, debug=True)
