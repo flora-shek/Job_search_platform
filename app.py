6beab37971a93b5b0fdde81d013b70cb3cfd9840
Shekinah Florance <84413589+flora-shek@users.noreply.github.com>
2024-04-20 14:42:33 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 from flask import Flask, jsonify, render_template
 
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
-
-
   
 JOBS = load_jobs_from_db()
 
 @app.route("/")
 def hello_jovian():
   
-  return render_template("index.html", jobs=JOBS, company_name='Jovian')
+  return render_template("index.html", jobs=JOBS)
 
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found",404
+  return render_template("jobpage.html",job = job)
   
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,20 +1,33 @@
 import pymysql
+
 import os
 timeout = 10
 connection = pymysql.connect(
     charset="utf8mb4",
     connect_timeout=timeout,
     cursorclass=pymysql.cursors.DictCursor,
-    db=os.environ['db'],
-    host= os.environ['host'],
-    password= os.environ['password'],
-    read_timeout=timeout,
-    port=int(os.environ['port']),
-    user=os.environ['user'],
-    write_timeout=timeout,
+    db = os.environ['db'],
+    host = os.environ['host'],
+    password = os.environ['password'],
+    read_timeout = timeout,
+    port = int(os.environ['port']),
+    user = os.environ['user'],
+    write_timeout = timeout,
 )
 def load_jobs_from_db():
   cursor = connection.cursor()
-  result = cursor.execute("select * from joviancareers.jobs")
+  result = cursor.execute("select * from jobs")
   result_all = cursor.fetchall()
   return result_all
+def load_job_from_db(id):
+  cursor = connection.cursor()
+  sql = "SELECT * FROM `jobs` WHERE `id`=%s "
+  result = cursor.execute(sql,(id))
+  result_all = cursor.fetchall()
+  if not result_all:
+    return None
+  else:
+    return result_all[0]
+
+
+ 
