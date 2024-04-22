0a9347b98ef200e6cfc31e75e39537a2dadf0631
Shekinah Florance <84413589+flora-shek@users.noreply.github.com>
2024-04-22 10:18:58 +0000

    final

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, render_template, request
 
-from database import load_jobs_from_db, load_job_from_db,add_application_to_db
+from database import load_jobs_from_db, load_job_from_db,add_application_to_db,load_applications_from_db
+
 from hcaptcha import verify_hcaptcha,site_key
 
-
 app = Flask(__name__)
-  
+
 JOBS = load_jobs_from_db()
 
 @app.route("/")
@@ -17,6 +17,13 @@ def hello_jovian():
 def list_jobs():
   return jsonify(JOBS)
 
+@app.route("/api/application/<id>")
+def show_application(id):
+  job = load_applications_from_db(id)
+  if not job:
+    return "Not Found",404
+  return jsonify(job)
+
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
@@ -25,7 +32,7 @@ def show_job(id):
     return "Not Found",404
   return render_template("jobpage.html",job = job,site_key = site_key)
   
-@app.route("/job/<id>/apply", methods=[ "post"])
+@app.route("/job/<id>/apply", methods=["post"])
 def apply_to_job(id):
   token = request.form.get('g-recaptcha-response')
   if verify_hcaptcha(token):
